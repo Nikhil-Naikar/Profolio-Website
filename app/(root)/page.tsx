725a44c35826ea2c80372c0f3cfd7390d3f156d4
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-11 02:10:44 -0400

    building home page, utilizing chakaraUI components like Box, playing arounf with the class props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 import { Link } from "@chakra-ui/next-js";
+import { Box, Container, Heading } from "@chakra-ui/react";
 import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello World, Welcome to my profolio website</h1>
-      <Link href="/about" color="blue.400" _hover={{ color: "blue.500" }}>
-        About
-      </Link>
-    </div>
+    <Container>
+      <Box bg="tomato" w="100%" p={3} mt={6} mt={6} borderRadius="lg" alignItems='center'>
+        <h1>Hello, I'm a Software Engineer.</h1>
+      </Box>
+      
+      <Box display={{md: 'flex'}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Nikhil Naikar
+          </Heading>
+          <p>Software Engineer (Full-Stack Developer / Cloud Engineer / Data Engineer)</p>
+        </Box>
+      </Box>
+
+
+    </Container>
   );
 };
 
