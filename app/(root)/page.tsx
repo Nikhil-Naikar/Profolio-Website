07d93c15000b694c2927abb24ca355ae6c69ceba
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-13 21:48:21 -0400

    added bio section, paragraph component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import NextLink from "next/link";
+import Paragraph from "@/components/Paragraph";
 import Section from "@/components/Section";
-import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { BioSection, BioYear } from "@/components/Bio";
 
 const Home = () => {
   return (
@@ -38,8 +42,47 @@ const Home = () => {
       </Box>
       <Section delay={0.1}>
         <Heading as="h3" variant="section-title">About Me</Heading>
-        <p>Paragraph</p>
+        <Paragraph>I enjoy tackling complex problems and collaborating with others to build innovative solutions. 
+          My passion lies in AI, cloud computing, and software architecture. I am committed to continuous learning 
+          and to ongoing professional development by staying updated with the latest tech trends and advancing my coding skills. 📈</Paragraph>
+        <Flex align="center" justify="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">My Profolio</Button>
+          </NextLink>
+        </Flex>
       </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">Bio</Heading>
+        <BioSection>
+          <BioYear>1999</BioYear>
+          Born in Kuwait
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Worked for Siemens Energy as Project Manager Intern
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Completed my Bachelor of Science Degree, majored in Electrical Engineering
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Worked for Benevity as Software Engineer Intern
+        </BioSection>
+        <BioSection>
+          <BioYear>2023</BioYear>
+          Completed my Master of Engineering Degree, specalized in Software Engineering
+        </BioSection>
+        <BioSection>
+          <BioYear>2024</BioYear>
+          Seeking entry level/ junior software engineering roles 
+        </BioSection>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">I ♡</Heading>
+        <Paragraph>Reading, Video Games, Music, Bloudering, Sushi, Coding, Travel</Paragraph>
+      </Section>
+
     </Container>
   );
 };
