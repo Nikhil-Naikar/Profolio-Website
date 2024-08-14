56d9745b3db6944bc2e2f1aeff322ff0760566d2
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-14 12:35:11 -0400

    checkout: make a GridItem component for neatly displaying my project info, added some projects images and data

--- app/(root)/works/page.tsx
+++ app/(root)/works/page.tsx
@@ -1,11 +1,44 @@
-import { Container, Heading } from "@chakra-ui/react";
+"use client"
+
+import { GridItem } from "@/components/GridItem";
+import Section from "@/components/Section";
+import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
 
 const Works = () => {
   return (
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+     
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+
+      <SimpleGrid columns={[1,1,2]} gap={6}>
+        <Section>
+          <GridItem id="the-podcast-platfrom-project" thumbnail="/images/projects/the-podcast-platform.png" title="The Podcast Platfrom">
+            Currently building an app for hosting podcasts with AI-powered features, including text-to-multiple-voices functionality and AI-generated images, with OpenAI integration.
+          </GridItem>
+        </Section>
+        <Section>
+          <GridItem id="Clock-In-Clock-Out-App" thumbnail="/images/projects/CICO.png" title="Clock In Clock Out App">
+            Practiced API development by creating an application for restaurant businesses, allowing employees to easily clock in/out of their shifts and check their shift/pay history.
+          </GridItem>
+        </Section>
+        <Section>
+          <GridItem id="HNM-Clothing-Store" thumbnail="/images/projects/HNM.png" title="HNM Clothing Store">
+            Developed an e-commerce website where users can browse and purchase clothes, with Python and a focus on applying good relational database design practices for MySQL.
+          </GridItem>
+        </Section>
+        <Section>
+          <GridItem id="Airbnb-Price-Intelligence" thumbnail="/images/projects/Airbnb.png" title="Airbnb Price Intelligence">
+            Created an AI model to assist Airbnb hosts in setting competitive prices and to help customers evaluate pricing fairness.
+          </GridItem>
+        </Section>
+        <Section>
+          <GridItem id="IMDB-Sentiment-Analysis" thumbnail="/images/projects/NN.png" title="IMDB Sentiment Analysis">
+            Built a deep-learning model using Neutral Networks algorithm to predict the correct sentiment for IMBD movie reviews with a dataset of 50000 rows.
+          </GridItem>
+        </Section>
+      </SimpleGrid>
     </Container>
   );
 };
---
+++ components/GridItem.tsx
@@ -0,0 +1,53 @@
+import NextLink from 'next/link'
+import { Box, Text, LinkBox, LinkOverlay, Image } from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+
+
+interface Props{
+    id: string,
+    title: string,
+    category?: string,
+    thumbnail: string,
+    children: React.ReactNode
+}
+
+export const GridItem = ({
+  children,
+  category = 'works',
+  id,
+  title,
+  thumbnail
+}: Props) => (
+  <Box w="100%" textAlign="center">
+    <LinkBox
+      as={NextLink}
+      href={`/${category}/${id}`}
+      scroll={false}
+      cursor="pointer"
+    >
+      <Image
+        src={thumbnail}
+        alt={title}
+        className="grid-item-thumbnail"
+        objectFit='cover'
+        // placeholder="blur"
+      />
+      <LinkOverlay as="div" href={`/${category}/${id}`}>
+        <Text mt={2} fontSize={20}>
+          {title}
+        </Text>
+      </LinkOverlay>
+      <Text fontSize={14}>{children}</Text>
+    </LinkBox>
+  </Box>
+)
+
+export const GridItemStyle = () => (
+  <Global
+    styles={`
+      .grid-item-thumbnail {
+        border-radius: 12px;
+      }
+    `}
+  />
+)
